97a3af326683b408bdd611502c2cd2c8a482c929
LookAtMyCodeNow <paweljanuszewicz2022@gmail.com>
2023-08-31 12:47:12 +0200

    Fixing all Bugs and Validated HTML and CSS MarkUp.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 // "use strict";
 
 // TODO:uncomment below later when I put everything together into a ready project
-// const heroHamburger = document.querySelector("#hero-hamburger");
-// const heroList = document.querySelector("#hero-list");
+const heroHamburger = document.querySelector("#hero-hamburger");
+const heroList = document.querySelector("#hero-list");
 
-// heroHamburger.addEventListener("click", function () {
-//   heroList.classList.toggle("unvisible");
-// });
+heroHamburger.addEventListener("click", function () {
+  heroList.classList.toggle("unvisible-x");
+});
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
+
+// Start Section 2
